2d82a24eb45fb099ec6b56caf36fb03d53c892f5
sharifulb07 <sharifulb07@gmail.com>
2026-02-16 09:56:22 +0600

    parallel data fetching from frontend side

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-<<<<<<< HEAD
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-=======
-import nextTs from "eslint-config-next/typescript";
+export default defineConfig([
+  nextVitals,
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
->>>>>>> de194b931ce14fe79c1b0571dcb3c49cf3861f21
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
